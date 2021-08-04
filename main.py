e5c3798b0d89961aaa11e5f83d866c57323c31a6
gk <shinhwagk@outlook.com>
2021-08-04 09:53:04 +0800

    check

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def get_logger():
 
 logger = get_logger()
 
-EXECUTE_TIMEOUT = os.getenv('EXECUTE_TIMEOUT', '6000')
+EXECUTE_TIMEOUT = os.getenv('EXECUTE_TIMEOUT', '10000')
 
 
 class DatabasePool:
@@ -68,7 +68,15 @@ class DatabasePool:
     def __restrictSqlTextType(self, sqlText) -> bool:
         return False if re.match(r'(?i)^\s*select', sqlText) is None else True
 
+    def __exceptionProc(self, exc: cx_Oracle.Error, db_id: str = None):
+        error, = exc.args
+        if error.code == 'DPI-1010':
+            self.__closePool(db_id)
+
+        print("Oracle-Error-Code:", error.code)
+        print("Oracle-Error-Message:", error.message)
     # param sql_text 名字必须是sql_text 因为他和请求参数一致
+
     def query(self, db_id, sql_text: str, binds=()):
         # code 1 is error, 0 is success.
         result = {'code': 1, 'result': [], 'error': "", 'db_id': db_id}
@@ -80,24 +88,22 @@ class DatabasePool:
                     result['error'] = 'datasource not exist.'
                     logger.error("datasource not exist.")
                 else:
-                    conPool = dbObj['pool']
-                    con = conPool.acquire()
-                    dbObj['last'] = datetime.now()
+                    con = dbObj['pool'].acquire()
                     logger.info("%s start  query", db_id)
                     st = datetime.now()
+                    con.callTimeout = int(EXECUTE_TIMEOUT)  # milliseconds
                     try:
-                        time_out = int(EXECUTE_TIMEOUT)  # milliseconds
-                        con.callTimeout = time_out
                         with con.cursor() as cur:
                             cur.execute(sql_text, binds)
                             cur.rowfactory = self.__rowfactory(cur)
                             result['code'] = 0
                             result['result'] = cur.fetchall()
+                        dbObj['last'] = datetime.now()
                     except Exception as e:
-                        logger.error("db_id: %s :: %s", db_id, f'{str(e)}, sql_text: {base64.b64encode(sql_text.encode())}, binds:{json.dumps(binds)}')
+                        self.__exceptionProc(e, db_id)
                         raise
                     finally:
-                        conPool.release(con)
+                        dbObj['pool'].release(con)
                     logger.info("%s end query", db_id)
                     et = datetime.now() - st
                     logger.info("%s release conn", db_id)
@@ -109,35 +115,47 @@ class DatabasePool:
             result['error'] = 'sql_text only suppet [select]'
         return result
 
+    def __closePool(self, db_id):
+        do = self.__dbObjs.get(db_id)
+        if do is not None:
+            try:
+                do['pool'].close()
+            except Exception as e:
+                logger.error("db_id: %s :: %s", db_id, f'{str(e)}')
+
     def __closeLongUnusedDBObj(self):
         while True:
             time.sleep(60)
-            logger.info('%s :: %s', "system", f"try remove long time unused the session pool.")
+            logger.info('%s :: %s', "system", "try remove long time unused the session pool.")
             for db_id in list(self.__dbObjs):
                 do = self.__dbObjs.get(db_id)
                 if do is not None:
                     lastTime = do['last']
                     if lastTime + timedelta(minutes=10) <= datetime.now():
-                        conPool = do['pool']
                         try:
-                            conPool.close()
+                            do['pool'].close()
                         except Exception as e:
+                            self.__exceptionProc(e, db_id)
                             logger.error('%s :: %s', "system", f"db_id: {db_id} :: {str(e)}.")
                         finally:
                             del self.__dbObjs[db_id]
                         logger.info('%s :: %s', "system", f"db_id: {db_id} removed from database pool.")
 
     def __createConPool(self, db_id):
-        logger.info('%s :: %s', f"db_id: {db_id}", "create session pool to database pool.")
-        dss = self.__readDss()
-        ds = dss.get(db_id)
-        if ds is None:
-            raise Exception('datasource not exist.')
-        user = ds['user']
-        password = ds['password']
-        dsn = ds['dsn']
-        conPool = cx_Oracle.SessionPool(user=user, password=password, dsn=dsn, min=1, max=2, increment=1, timeout=10, encoding="UTF-8")
-        self.__dbObjs[db_id] = {'pool': conPool, 'last': datetime.now()}
+        try:
+            logger.info('%s :: %s', f"db_id: {db_id}", "create session pool to database pool.")
+            dss = self.__readDss()
+            ds = dss.get(db_id)
+            if ds is None:
+                raise Exception('datasource not exist.')
+            user = ds['user']
+            password = ds['password']
+            dsn = ds['dsn']
+            conPool = cx_Oracle.SessionPool(user=user, password=password, dsn=dsn, min=1, max=2, increment=1, timeout=10, encoding="UTF-8")
+            self.__dbObjs[db_id] = {'pool': conPool, 'last': datetime.now()}
+        except Exception as e:
+            self.__exceptionProc(e, db_id)
+            raise
 
     def __readDss(self):
         ch = os.getenv('CONSUL_HOST')
@@ -163,9 +181,6 @@ class DatabasePool:
                         "dsn": f"{db_ip}:{db_port}/{db_sn}"
                     }
             return dss
-        elif os.path.exists('dss.json'):
-            with open('dss.json', 'r') as f:
-                return json.load(f)
         else:
             return {}
 
@@ -184,12 +199,11 @@ class MultidatabaseHandler(BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header('Content-type', 'application/json')
             self.send_header('os-hostname', socket.gethostname())
-            self.send_header('content-encoding', 'gzip')
-
-            queryParams = self.rfile.read(int(self.headers.get('Content-Length', 0)))
-            reqObj = json.loads(queryParams)
 
             if self.path == '/query':
+                self.send_header('content-encoding', 'gzip')
+                queryParams = self.rfile.read(int(self.headers.get('Content-Length', 0)))
+                reqObj = json.loads(queryParams)
                 result = self.dbPool.query(**reqObj)
                 data = self.gzip_encode(json.dumps(result).encode('utf-8'))
                 self.send_header('content-length', len(data))
