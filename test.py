3e3fa5a0d4c749059001e9630b07085ae9316c8c
gk <shinhwagk@outlook.com>
2022-03-29 14:31:55 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,14 @@
+import logging
+from database import Database
+from database_pool import DatabasePool
+
+
+# db = Database('system', 'sys123SYS', '10.65.193.12:1521/whpay')
+# print(db.query('select * from dual'))
+
+
+# db_pool = DatabasePool('system', 'sys123SYS')
+# print(db_pool.query('10.65.193.121:1521/whpay', 'select * from dual'))
+
+
+logging.error("sss")
